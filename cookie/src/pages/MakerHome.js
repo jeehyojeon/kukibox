47eebb147cfdc5b3c09ed3a5f04b36d0497afd46
전지효 <cordelia0626@naver.com>
2023-09-22 23:52:12 +0900

    makerhome

--- cookie/src/pages/MakerHome.js
+++ cookie/src/pages/MakerHome.js
@@ -8,13 +8,18 @@ function MakerHome() {
       <Title title={`공연 정보 작성\n*`} />
       <br />
       <div className="maker_home_name">공연 이름</div>
-      <div className="maker_home_namebox"></div>
+      <div className="maker_home_namebox">
+        
+      </div>
       <div className="maker_home_link">링크 첨부</div>
       <div className="maker_home_linkbox"></div>
       <div className="maker_home_option">선택 작성</div>
       <div className="maker_home_optionbox"></div>
 
-      <div className="maker_home_enroll">등록하기</div>
+      <div className="maker_home_enroll">
+        등록하기
+        <div className="maker_home_enrollbox"></div>
+      </div>
     </div>
   );
 }
--- cookie/src/pages/MakerIntro.js
+++ cookie/src/pages/MakerIntro.js
@@ -1,19 +1,32 @@
 import React from "react";
 import "./MakerIntro.css";
-import MakerIntroKuki from "../assets/images/MakerIntroKuki.svg";
+import MakerIntroKuki from "../assets/images/kuki1.svg";
 import MakerIntroBox from "../assets/images/MakerIntroBox.svg";
 
 function MakerIntro() {
+  const kukipos = {
+    position: "absolute",
+    top: "50px",
+    left: "220px"
+    };
+  const boxpos = {
+    position: "absolute",
+    top: "397px",
+    left: "45px"
+  };
+  
+
   return (
     <div className="maker_intro">
-      <div className="maker_intro_title">공연 정보 작성</div>
-      <div className="maker_intro_kuki">
-        <img src={MakerIntroKuki} alt="MakerIntroKuki" />
-      </div>
+      <span className="maker_intro_title">쿠키박스
+        <span className="maker_intro_kuki">
+          <img src={MakerIntroKuki} alt="MakerIntroKuki" width={30} height={30} style={kukipos} />
+        </span>
+      </span>
       <div className="maker_intro_subbox"></div>
       <div className="maker_intro_subtitle">공연 관계자용</div>
       <div className="maker_intro_box">
-        <img src={MakerIntroBox} alt="MakerIntroBox" />
+        <img src={MakerIntroBox} alt="MakerIntroBox" style={boxpos} />
       </div>
     </div>
   );
