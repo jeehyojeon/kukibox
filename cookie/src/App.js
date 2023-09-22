d9bec5ae18a9aa766e39e3db77722395e32cd8c6
devBuzz142 <devbuzz142@gmail.com>
2023-09-22 18:54:51 +0900

    feat: routing init

--- cookie/src/App.js
+++ cookie/src/App.js
@@ -4,11 +4,17 @@ import "./App.css";
 import MakerHome from "./pages/MakerHome.js";
 import AudIntro from "./pages/AudIntro";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <AudIntro />
-      <MakerHome />
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<AudIntro />} /> */}
+          <Route path="/" element={<MakerHome />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
