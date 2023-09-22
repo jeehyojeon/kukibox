7f6047f369408cf1714499c42b6b9ac9c2b47ee3
devBuzz142 <devbuzz142@gmail.com>
2023-09-23 01:48:37 +0900

    feat: kuki 가져오기 api 연결

--- cookie/src/pages/KukiBoxHomePage.js
+++ cookie/src/pages/KukiBoxHomePage.js
@@ -1,24 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./KukiBoxHomePage.css";
 import { useParams } from "react-router-dom";
 
 function KukiBoxHomePage() {
   const { boxId } = useParams();
+  const [kukies, setKukies] = useState([]);
 
   useEffect(() => {
     const getKukiList = async () => {
-      const response = await fetch(`http://localhost:3030/kuki`);
+      const response = await fetch(`http://localhost:3030/box/${boxId}/kukies`);
       const data = await response.json();
-      console.log(data);
+
+      setKukies([...data]);
     };
 
-    // getKukiList();
+    getKukiList();
   }, [boxId]);
 
   return (
     <div className="KukiBoxHomePage page">
       <div className="kukibox_section">
-        <div className="kukibox">kukibox</div>
+        <div className="kukibox">
+          {kukies.map((kuki) => (
+            <div key={`kuki` + kuki.id}>kuki {kuki.id}</div>
+          ))}
+        </div>
         <div className="kukibox_button">
           <button className="left_button"></button>
           <button className="right_button"></button>
