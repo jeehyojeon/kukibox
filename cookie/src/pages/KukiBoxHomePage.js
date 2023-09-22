954c4a1c0191bad64e0f36a07f7f53892b9747e8
devBuzz142 <devbuzz142@gmail.com>
2023-09-23 01:40:37 +0900

    feat: 쿠키홈페이지 배경 스타일링 거의 완료

--- cookie/src/pages/KukiBoxHomePage.js
+++ cookie/src/pages/KukiBoxHomePage.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./KukiBoxHomePage.css";
+import { useParams } from "react-router-dom";
 
 function KukiBoxHomePage() {
+  const { boxId } = useParams();
+
+  useEffect(() => {
+    const getKukiList = async () => {
+      const response = await fetch(`http://localhost:3030/kuki`);
+      const data = await response.json();
+      console.log(data);
+    };
+
+    // getKukiList();
+  }, [boxId]);
+
   return (
     <div className="KukiBoxHomePage page">
       <div className="kukibox_section">
         <div className="kukibox">kukibox</div>
-        <button>left</button>
-        <button>right</button>
+        <div className="kukibox_button">
+          <button className="left_button"></button>
+          <button className="right_button"></button>
+        </div>
       </div>
       <div className="make_button_section">
         <button className="make_button">쿠키 담기</button>
