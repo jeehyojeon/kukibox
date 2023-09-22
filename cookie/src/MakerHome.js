fcb1855c7d849119831446c087ea0377b8544ae3
전지효 <cordelia0626@naver.com>
2023-09-22 14:59:22 +0900

    Thu 15:00

---
+++ cookie/src/MakerHome.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./MakerHome.css";
+
+function MakerHome() {
+
+  return (
+    <div className="maker_home">
+      <div className="maker_home_title">
+        공연 정보 작성<br />
+        *
+      </div>
+      <div classname="maker_home_name">
+        공연 이름
+      </div>
+      <div classname="maker_home_namebox"></div>
+      <div classname="maker_home_link">
+        링크 첨부
+      </div>
+      <div classname="maker_home_linkbox"></div>
+      <div classname="maker_home_option">
+        선택 작성
+      </div>
+      <div classname="maker_home_optionbox"></div>
+
+      <div classname="maker_home_enroll">
+        등록하기
+      </div>
+    </div>
+  );
+}
+
+export default MakerHome;
--- cookie/src/MakerIntro.js
+++ cookie/src/MakerIntro.js
@@ -4,15 +4,7 @@ import MakerIntroKuki from "./MakerIntroKuki.svg";
 import MakerIntroBox from "./MakerIntroBox.svg";
 
 function MakerIntro() {
-  const subbox = {
-    width: "111px",
-    height: "26px",
-    top: "241px",
-    left: "140px",
-    borderRadius: "50px",
-    backgroundColor: "#F6EDDD",
-    position: "absolute", // 위치 지정을 위한 설정
-  };
+  
   return (
     <div className="maker_intro">
       <div className="maker_intro_title">
@@ -21,7 +13,7 @@ function MakerIntro() {
       <div className="maker_intro_kuki">
         <img src={MakerIntroKuki} alt="MakerIntroKuki" />
       </div>
-      <div style={subbox}></div>
+      <div className="maker_intro_subbox"></div>
       <div className="maker_intro_subtitle">
         공연 관계자용
       </div>
