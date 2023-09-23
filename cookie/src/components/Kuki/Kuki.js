3e0d978cf3ff1a75274dc6795dafee450e0bb2d4
devBuzz142 <devbuzz142@gmail.com>
2023-09-23 15:49:37 +0900

    feat: flip!

--- cookie/src/components/Kuki/Kuki.js
+++ cookie/src/components/Kuki/Kuki.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Kuki.css";
 
-import { KUKI_FRONT } from "../../assets/images/kuki.js";
+import { KUKI_BACK, KUKI_FRONT } from "../../assets/images/kuki.js";
 
-function Kuki({ style = 1, isFlipped = false, isPublic = true }) {
+function Kuki({ style = 1, isPublic = true, content = "" }) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleKukiClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+
   return (
-    <div className="kuki hoverable">
-      <img src={KUKI_FRONT[style]} width={110} height={110} />
+    <div
+      className={`kuki hoverable ${isFlipped ? "isFlipped" : ""}`}
+      onClick={handleKukiClick}>
+      <img className="front" src={KUKI_FRONT[style]} width={110} height={110} />
+      <img className="back" src={KUKI_BACK[style]} width={110} height={110} />
     </div>
   );
 }
