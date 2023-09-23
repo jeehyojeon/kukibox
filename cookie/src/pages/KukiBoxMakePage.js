756d741f79c68565e142f0708b574e3509933020
devBuzz142 <devbuzz142@gmail.com>
2023-09-23 17:31:04 +0900

    feat: textarea

--- cookie/src/pages/KukiBoxMakePage.js
+++ cookie/src/pages/KukiBoxMakePage.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./KukiBoxMakePage.css";
 
 function KukiBoxMakePage() {
+  const [content, setContent] = useState("");
+
+  const onContentChange = (event) => {
+    setContent(event.target.value);
+  };
+
   return (
     <div className="page">
       <div className="page_header">쿠키를 선택해 주세요!</div>
       <div className="design_section"></div>
-      <div className="content_section"></div>
+      <div className="content_section">
+        <textarea
+          className="content_textarea"
+          placeholder="자유롭게 작성해주세요."
+          value={content}
+          onChange={onContentChange}
+        />
+      </div>
       <div className="public_section"></div>
       <div className="button_section"></div>
     </div>
