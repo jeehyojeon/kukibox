ab629e26fc77b87808c3074c162fd35b0b5cb9fb
devBuzz142 <devbuzz142@gmail.com>
2023-09-23 17:23:12 +0900

    feat: section positioning

--- cookie/src/pages/KukiBoxMakePage.js
+++ cookie/src/pages/KukiBoxMakePage.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+import "./KukiBoxMakePage.css";
+
 function KukiBoxMakePage() {
   return (
-    <div>
-      <h1>KukiBoxMakePage</h1>
+    <div className="page">
+      <div className="page_header">쿠키를 선택해 주세요!</div>
+      <div className="design_section"></div>
+      <div className="content_section"></div>
+      <div className="public_section"></div>
+      <div className="button_section"></div>
     </div>
   );
 }
