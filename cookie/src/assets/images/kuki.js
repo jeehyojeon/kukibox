a58ae931d82811a6c632fe6b8e0f055295da8705
devBuzz142 <devbuzz142@gmail.com>
2023-09-23 15:32:11 +0900

    feat: kuki_back image file

--- cookie/src/assets/images/kuki.js
+++ cookie/src/assets/images/kuki.js
@@ -1,7 +1,29 @@
-import kuki1 from "./kuki1.svg";
-import kuki2 from "./kuki2.svg";
-import kuki3 from "./kuki3.svg";
-import kuki4 from "./kuki4.svg";
-import kuki5 from "./kuki5.svg";
+import kuki1_front from "./kuki1_front.svg";
+import kuki2_front from "./kuki2_front.svg";
+import kuki3_front from "./kuki3_front.svg";
+import kuki4_front from "./kuki4_front.svg";
+import kuki5_front from "./kuki5_front.svg";
 
-export const KUKI_STYLE = [kuki1, kuki1, kuki2, kuki3, kuki4, kuki5];
+import kuki1_back from "./kuki1_back.svg";
+import kuki2_back from "./kuki2_back.svg";
+import kuki3_back from "./kuki3_back.svg";
+import kuki4_back from "./kuki4_back.svg";
+import kuki5_back from "./kuki5_back.svg";
+
+export const KUKI_FRONT = [
+  kuki1_front,
+  kuki1_front,
+  kuki2_front,
+  kuki3_front,
+  kuki4_front,
+  kuki5_front,
+];
+
+export const KUKI_BACK = [
+  kuki1_back,
+  kuki1_back,
+  kuki2_back,
+  kuki3_back,
+  kuki4_back,
+  kuki5_back,
+];
--- cookie/src/components/Kuki/Kuki.js
+++ cookie/src/components/Kuki/Kuki.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Kuki.css";
 
-import { KUKI_STYLE } from "../../assets/images/kuki.js";
+import { KUKI_FRONT } from "../../assets/images/kuki.js";
 
 function Kuki({ style = 1, isFlipped = false, isPublic = true }) {
   return (
     <div className="kuki hoverable">
-      <img src={KUKI_STYLE[style]} width={110} height={110} />
+      <img src={KUKI_FRONT[style]} width={110} height={110} />
     </div>
   );
 }
